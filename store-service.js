22642b33f30bc0673c81de48f223b468a95bc989
Avneet Kaur <106433236+avneet-kaur9@users.noreply.github.com>
2023-06-16 22:21:01 -0400

    Update store-service.js

--- store-service.js
+++ store-service.js
@@ -4,93 +4,119 @@ const path = require("path");
 let items = [];
 let categories = [];
 
-function initialize() 
-{
-  return new Promise((resolve, reject) => 
-  {
-    const i_Path = path.join(__dirname, "data", "items.json");
+function initialize() {
+  return new Promise((resolve, reject) => {
+    const itemsFilePath = path.join(__dirname, "data", "items.json");
     const categoriesFilePath = path.join(__dirname, "data", "categories.json");
 
-    fs.readFile(i_Path, "utf8", (err, i_data) => {
-      if (err) 
-      {
+    fs.readFile(itemsFilePath, "utf8", (err, itemsData) => {
+      if (err) {
         reject("Unable to read file");
-      
         return;
       }
 
-      items = JSON.parse(i_data);
+      items = JSON.parse(itemsData);
 
-      fs.readFile(categoriesFilePath, "utf8", (err, c_data) => 
-      {
-        if (err) 
-        {
+      fs.readFile(categoriesFilePath, "utf8", (err, categoriesData) => {
+        if (err) {
           reject("Unable to read file");
-        
           return;
         }
 
-        categories = JSON.parse(c_data);
-        
+        categories = JSON.parse(categoriesData);
         resolve();
       });
     });
   });
 }
 
-function getAllItemss() 
-{
-  return new Promise((resolve, reject) => 
-  {
-    if (items.length === 0) 
-    {
+function getAllItems() {
+  return new Promise((resolve, reject) => {
+    if (items.length === 0) {
       reject("No results returned");
-    
       return;
     }
-    
     resolve(items);
   });
 }
 
-function getPublishedItems() 
-{
-  return new Promise((resolve, reject) => 
-  {
+function getPublishedItems() {
+  return new Promise((resolve, reject) => {
     const publishedItems = items.filter((item) => item.published === true);
-  
-    if (publishedItems.length === 0) 
-    {
+    if (publishedItems.length === 0) {
       reject("No results returned");
-    
       return;
     }
-
-
-
-    
-    
     resolve(publishedItems);
   });
 }
 
-function getCategories() 
-{
-  return new Promise((resolve, reject) => 
-  {
-    if (categories.length === 0) 
-    {
+function getCategories() {
+  return new Promise((resolve, reject) => {
+    if (categories.length === 0) {
       reject("No results returned");
-    
       return;
     }
-    
     resolve(categories);
   });
 }
 
+function addItem(itemData) {
+  return new Promise((resolve, reject) => {
+    itemData.published = itemData.published || false;
+
+    itemData.id = items.length + 1;
+    itemData.postDate = new Date().toISOString().slice(0, 10);
+    items.push(itemData);
+
+    resolve(itemData);
+  });
+}
+
+function getItemsByCategory(category) {
+  return new Promise((resolve, reject) => {
+    const itemsByCategory = items.filter((item) => item.category == category);
+    if (itemsByCategory.length === 0) {
+      reject("No results returned");
+    } else {
+      resolve(itemsByCategory);
+    }
+  });
+}
+
+function getItemsByMinDate(minDateStr) {
+  return new Promise((resolve, reject) => {
+    const itemsByMinDate = items.filter(
+      (item) => new Date(item.postDate) >= new Date(minDateStr)
+    );
+    if (itemsByMinDate.length === 0) {
+      reject("No results returned");
+    } else {
+      resolve(itemsByMinDate);
+    }
+  });
+}
+
+function getItemById(id) {
+  return new Promise((resolve, reject) => {
+    const item = items.find((item) => item.id == id);
+    if (item) {
+      resolve(item);
+    } else {
+      reject("No result returned");
+    }
+  });
+}
+
 const storeService = {
-  initialize, getAllItemss, getPublishedItems, getCategories,
+  initialize,
+  getAllItems,
+  getPublishedItems,
+  getCategories,
+  addItem,
+  getItemsByCategory,
+  getItemsByMinDate,
+  getItemById,
 };
 
 module.exports = storeService;
