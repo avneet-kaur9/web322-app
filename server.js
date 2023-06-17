5f47fadd4ec5813d011a80542862613c754537be
Avneet Kaur <106433236+avneet-kaur9@users.noreply.github.com>
2023-06-16 21:50:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,76 +1,73 @@
 /*********************************************************************************
 
-WEB322 – Assignment 02
-I declare that this assignment is my own work in accordance with Seneca  Academic 
-Policy.  No part *  of this assignment has been copied manually or electronically 
-from any other source (including 3rd party web sites) or distributed to other 
-students.
-
+WEB322 – Assignment 03
+I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part * of this assignment has
+been copied manually or electronically from any other source (including 3rd party web sites) or distributed to other students.
 Name: Avneet Kaur
 Student ID: 164275216
-Date: 02/06/2023
-Cyclic Web App URL: https://dull-red-haddock-hat.cyclic.app
-I don't why my cyclic link is not working but you can check my work on github,
-I left it public.
-GitHub Repository URL: https://github.com/avneet-kaur9/web322-app
+Date: 12/06/2023
+Cyclic Web App URL: https://helpful-slip-ox.cyclic.app
+GitHub Repository URL: https://github.com/Kirandeep247/web322-app.git
 
 ********************************************************************************/
-const express = require("express");
+
+const express = require("express"); //express module
 const path = require("path");
 const store = require("./store-service");
-
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080; //listening
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-  res.redirect("/about");
+app.get("/", (r1, r2) => 
+{
+  r2.redirect("/about"); //retrieving content from about,html
 });
 
-app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/views/about.html");
+app.get("/about", (r1, r2) => 
+{
+  r2.sendFile(__dirname + "/views/about.html"); //sending content a s a response
 });
 
-// GET route for "/shop"
-app.get("/shop", (req, res) => {
+app.get("/shop", (r1, r2) =>  // for "/shop"
+{
   store
     .getPublishedItems()
     .then((categories) => {
-      res.status(200).json(categories);
+      r2.status(200).json(categories);
     })
     .catch((err) => {
-      res.status(500).json({ message: err });
+      r2.status(500).json({ message: err });
     });
 });
 
-// GET route for "/items"
-app.get("/items", (req, res) => {
+app.get("/items", (r1, r2) =>  // for "/items"
+{
   store
-    .getAllItems()
+    .getAllItemss()
     .then((items) => {
-      res.status(200).json(items);
+      r2.status(200).json(items);
     })
     .catch((err) => {
-      res.status(500).json({ message: err });
+      r2.status(500).json({ message: err });
     });
 });
 
-// GET route for "/categories"
-app.get("/categories", (req, res) => {
+app.get("/categories", (r1, r2) => // for "/categories"
+{
   store
     .getCategories()
     .then((categories) => {
-      res.status(200).json(categories);
+      r2.status(200).json(categories);
     })
     .catch((err) => {
-      res.status(500).json({ message: err });
+      r2.status(500).json({ message: err });
     });
 });
 
-// 404 route handler
-app.use((req, res) => {
-  res.status(404).send("Page Not Found");
+app.use((r1, r2) => // for "404"
+{
+  r2.status(404).send("Page Not Found");
 });
 
 store
