55d74cad50a450d68ed9fbe50749566ddd6c5172
Avneet Kaur <106433236+avneet-kaur9@users.noreply.github.com>
2023-06-16 22:20:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,64 +10,145 @@ Cyclic Web App URL: https://rich-plum-bull-coat.cyclic.app/
 GitHub Repository URL: https://github.com/avneet-kaur9/web322-app.git
 
 ********************************************************************************/
+const express = require("express");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
 
-const express = require("express"); //express module
-const path = require("path");
 const store = require("./store-service");
+
 const app = express();
-const PORT = process.env.PORT || 8080; //listening
+const PORT = process.env.PORT || 8080;
+
+// configure cloudinary
+cloudinary.config({
+  cloud_name: "dixrvpqpe",
+  api_key: "311594855559177",
+  api_secret: "r_7xWtleyeog_BmZxNJRQoYeifQ",
+  secure: true,
+});
+
+const upload = multer();
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (r1, r2) => 
-{
-  r2.redirect("/about"); //retrieving content from about,html
+app.get("/", (req, res) => {
+  res.redirect("/about");
 });
 
-app.get("/about", (r1, r2) => 
-{
-  r2.sendFile(__dirname + "/views/about.html"); //sending content a s a response
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
 });
 
-app.get("/shop", (r1, r2) =>  // for "/shop"
-{
+// GET route for "/shop"
+app.get("/shop", (req, res) => {
   store
     .getPublishedItems()
     .then((categories) => {
-      r2.status(200).json(categories);
+      res.status(200).json(categories);
     })
     .catch((err) => {
-      r2.status(500).json({ message: err });
+      res.status(500).json({ message: err });
     });
 });
 
-app.get("/items", (r1, r2) =>  // for "/items"
-{
-  store
-    .getAllItemss()
+// GET route for "/items"
+app.get("/items", (req, res) => {
+  const { category, minDate } = req.query;
+  let promise;
+
+  if (category) {
+    promise = store.getItemsByCategory(category);
+  } else if (minDate) {
+    promise = store.getItemsByMinDate(minDate);
+  } else {
+    promise = store.getAllItems();
+  }
+
+  promise
     .then((items) => {
-      r2.status(200).json(items);
+      res.json(items);
     })
     .catch((err) => {
-      r2.status(500).json({ message: err });
+      res.json({ message: err });
     });
 });
 
-app.get("/categories", (r1, r2) => // for "/categories"
-{
+// GET route for "/categories"
+app.get("/categories", (req, res) => {
   store
     .getCategories()
     .then((categories) => {
-      r2.status(200).json(categories);
+      res.status(200).json(categories);
     })
     .catch((err) => {
-      r2.status(500).json({ message: err });
+      res.status(500).json({ message: err });
     });
 });
 
-app.use((r1, r2) => // for "404"
-{
-  r2.status(404).send("Page Not Found");
+// show add items form
+app.get("/items/add", (req, res) => {
+  res.sendFile(__dirname + "/views/addItem.html");
+});
+
+// submitting add item form
+app.post("/items/add", upload.single("featureImage"), (req, res) => {
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream((error, result) => {
+          if (result) {
+            resolve(result);
+          } else {
+            reject(error);
+          }
+        });
+
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+
+    async function upload(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+      return result;
+    }
+
+    upload(req).then((uploaded) => {
+      processItem(uploaded.url);
+    });
+  } else {
+    processItem("");
+  }
+
+  function processItem(imageUrl) {
+    req.body.featureImage = imageUrl;
+
+    store
+      .addItem(req.body)
+      .then(() => {
+        res.redirect("/items");
+      })
+      .catch((err) => {
+        res.status(500).json({ message: err });
+      });
+  }
+});
+
+app.get("/item/:value", (req, res) => {
+  store
+    .getItemById(req.params.value)
+    .then((item) => {
+      res.json(item);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
+// 404 route handler
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
 });
 
 store
